e89f4c276b09d7dc48df5c972a198c346463b39b
Yuriy Kaplun <kapluny777@gmail.com>
2018-10-09 15:16:13 +0300

    add first components

---
+++ UI/flights-search/app/components/e-search/component.ts
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+import { tagName } from '@ember-decorators/component';
+
+// @ts-ignore -- need to generate style modules
+import style from './style';
+
+@tagName('')
+export default class ESearch extends Component {
+  style = style;
+}
