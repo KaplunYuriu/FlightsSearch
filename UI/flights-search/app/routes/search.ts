5c6b71f2c491be2a09b30b466fdb56ded2478f06
Yuriy Kaplun <kapluny777@gmail.com>
2018-10-09 18:32:15 +0300

    add airports loading

--- UI/flights-search/app/routes/search.ts
+++ UI/flights-search/app/routes/search.ts
@@ -1,12 +1,22 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember-decorators/service';
+import _ from 'lodash';
+import { loadAirports } from 'flights-search/components/e-search/reducer';
 
 export default class Index extends Route {
-  renderTemplate(controller, model) {
-    const orderController = this.controllerFor('search');
+  @service redux;
 
-    return this.render('search', {
-      outlet: 'search',
-      controller,
-    });
+  renderTemplate(controller, model) {
+    const state = this.redux.getState();
+    const dataMustBeRefreshed = _.isEmpty(state.airports.airports);
+
+    if (dataMustBeRefreshed) {
+      return this.redux.dispatch(loadAirports()).then(() => {
+        return this.render('search', {
+          outlet: 'search',
+          controller,
+        })
+      });
+    }
   }
 }
