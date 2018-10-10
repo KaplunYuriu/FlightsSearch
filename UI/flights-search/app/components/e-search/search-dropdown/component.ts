3b0b9ab81baa44c92300cf63d69e7c26a042a42c
Yuriy Kaplun <kaplun.yuriy@gmail.com>
2018-10-10 20:49:50 +0300

    more styling

--- UI/flights-search/app/components/e-search/search-dropdown/component.ts
+++ UI/flights-search/app/components/e-search/search-dropdown/component.ts
@@ -1,13 +1,26 @@
 import Component from '@ember/component';
 
 import { tagName } from '@ember-decorators/component';
+import { action, computed } from '@ember-decorators/object';
 
 // @ts-ignore -- need to generate style modules
 import style from './style';
 import { Airport } from 'flights-search/components/e-search/reducer';
+import _ from 'lodash';
 
 @tagName('')
 export default class SearchDropdown extends Component {
   style = style;
   airports: Airport[];
+
+  _airport: Airport;
+
+  didReceiveAttrs() {
+    this.set('_airport', this.airports[0]);
+  }
+
+  @action
+  updateAirport(airport) {
+    this.set('_airport', airport);
+  }
 }
