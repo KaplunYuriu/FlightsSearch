8b6fef649fd2fe2575e2e73ac7075684f89d5b48
Yuriy Kaplun <kapluny777@gmail.com>
2018-10-10 13:46:16 +0300

    Add tabs structure

---
+++ UI/flights-search/app/components/e-search/tab-holder/component.ts
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+
+import { tagName } from '@ember-decorators/component';
+
+// @ts-ignore -- need to generate style modules
+import style from './style';
+import { Airport } from 'flights-search/components/e-search/reducer';
+
+@tagName('')
+export default class EApplicationTabHolder extends Component {
+  style = style;
+
+  airports: Airport[];
+}
---
+++ UI/flights-search/tests/components/e-application/tab-holder/component-test.ts
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+import { find } from '@ember/test-helpers';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+
+module('e-application/tab-holder', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await this.render(hbs`{{e-application/tab-holder}}`);
+
+    assert.dom('[data-test-e-application/tab-holder]').exists();
+
+    await a11yAudit({});
+    assert.ok(true, 'no a11y errors found!');
+  });
+});
